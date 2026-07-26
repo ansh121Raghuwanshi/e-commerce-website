8fa1053fd11ec6931d18895f0a0e11005bdc53ab
Ansh Raghuwanshi <patelansh7225@gmail.com>
2026-07-26 23:27:56 +0530

    Update CORS origin for admin frontend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://e-commerce-frontend-zvkp.onrender.com" , "http://localhost:5174"],
+ origin:["https://e-commerce-frontend-zvkp.onrender.com" , "https://e-commerce-admin-izec.onrender.com"],
  credentials:true
 }))
 
